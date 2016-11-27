7fb8a0fe1af10a685627c215a6bea416d25079a0
alonameir <alona9456@walla.com>
2016-11-27 03:01:05 -0800

    after pull

--- src/reviiyot.cpp
+++ src/reviiyot.cpp
@@ -9,23 +9,23 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-//	char* configurationFile = argv[1];
+	char* configurationFile = argv[1];
 
- //   Game game = Game(configurationFile);
-//	game.init();
-// 	Game initializedGame = game;
-// 	game.play();
-//
-//	cout << std::endl;
-// 	game.printWinner();
-//	game.printNumberOfTurns();
-//	cout << "----------" << endl;
-//	cout<<"Initial State:"<<endl;
-//  	initializedGame.printState();
-//	cout<<"----------"<<endl;
-//	cout<<"Final State:"<<endl;
-//	game.printState();
-//	return 0;
+    Game game = Game(configurationFile);
+	game.init();
+ 	//Game initializedGame = game;
+ 	game.play();
+
+	cout << std::endl;
+ 	game.printWinner();
+	game.printNumberOfTurns();
+	cout << "----------" << endl;
+	cout<<"Initial State:"<<endl;
+  //	initializedGame.printState();
+	cout<<"----------"<<endl;
+	cout<<"Final State:"<<endl;
+	game.printState();
+	return 0;
 
 	/*FigureCard* test=new FigureCard(Diamond,Jack);
 	cout  << (*test).toString() << endl;
@@ -33,15 +33,15 @@ int main(int argc, char **argv) {
 	NumericCard* testNum=new NumericCard(Spade, 1000000);
 	cout << (*testNum).toString() << endl;
 */
-	string* s= new string("KC QH 3D AH JH 2C 3S KS AS JS 3C KH AD QC JD QS 3H KD AC JC 2D 2H 2S QD");
-    //string* s= new string("KC QH 3D");
-    Deck d();
-    (d).buildDeck(*s, 3);
-	cout << (d).toString()<<endl;
+//	string* s= new string("KC QH 3D AH JH 2C 3S KS AS JS 3C KH AD QC JD QS 3H KD AC JC 2D 2H 2S QD");
+//    //string* s= new string("KC QH 3D");
+//    Deck* d=new Deck();
+//    (*d).buildDeck(*s, 3);
+//	cout << (*d).toString()<<endl;
 
-    Deck d2(d);
+   // Deck d2(d);
 
-    cout <<d2.toString() <<endl;
+    //cout <<d2.toString() <<endl;
 
 //	Hand* h= new Hand();
 
